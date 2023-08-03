2ce60cb83b0faddce4375f8d7122c0a561bdf11b
haizellatte <hyoeun5000@naver.com>
2023-08-03 14:59:00 +0900

    git commit -m font 확장자 변경

--- src/styles/GlobalFont.ts
+++ src/styles/GlobalFont.ts
@@ -5,13 +5,13 @@ import NotoSansKR_Regular from '../Assets/Fonts/NotoSansKR_Regular.otf';
 import NotoSansKR_Medium from '../Assets/Fonts/NotoSansKR_Medium.otf';
 import NotoSansKR_Bold from '../Assets/Fonts/NotoSansKR_Bold.otf';
 import NotoSansKR_Black from '../Assets/Fonts/NotoSansKR_Black.otf';
-import Pretendard_Thin from "../Assets/Fonts/Pretendard-Thin.otf";
-import Pretendard_ExtraLight from "../Assets/Fonts/Pretendard_ExtraLight.otf";
-import Pretendard_Light from "../Assets/Fonts/Pretendard_Light.otf";
-import Pretendard_Regular from "../Assets/Fonts/Pretendard_Regular.otf";
-import Pretendard_Medium from "../Assets/Fonts/Pretendard_Medium.otf";
-import Pretendard_SemiBold from "../Assets/Fonts/Pretendard_SemiBold.otf";
-import Pretendard_ExtraBold from "../Assets/Fonts/Pretendard_ExtraBold.otf";
+import Pretendard_Thin from "../Assets/Fonts/Pretendard_Thin.woff";
+import Pretendard_ExtraLight from "../Assets/Fonts/Pretendard_ExtraLight.woff";
+import Pretendard_Light from "../Assets/Fonts/Pretendard_Light.woff";
+import Pretendard_Regular from "../Assets/Fonts/Pretendard_Regular.woff";
+import Pretendard_Medium from "../Assets/Fonts/Pretendard_Medium.woff";
+import Pretendard_SemiBold from "../Assets/Fonts/Pretendard_SemiBold.woff";
+import Pretendard_ExtraBold from "../Assets/Fonts/Pretendard_ExtraBold.woff";
 
 export default createGlobalStyle`
   @font-face {
@@ -46,37 +46,37 @@ export default createGlobalStyle`
   }
   @font-face {
     font-family: "Pretendard_Thin";
-    src: url("Pretendard"), url(${Pretendard_Thin}) format('otf');
+    src: url("Pretendard"), url(${Pretendard_Thin}) format('woff');
     font-weight: 100;
   }
   @font-face {
     font-family: "Pretendard_ExtraLight";
-    src: url("Pretendard"), url(${Pretendard_ExtraLight}) format('otf');
+    src: url("Pretendard"), url(${Pretendard_ExtraLight}) format('woff');
     font-weight: 200;
   }
   @font-face {
     font-family: "Pretendard_Light";
-    src: url("Pretendard"), url(${Pretendard_Light}) format('otf');
+    src: url("Pretendard"), url(${Pretendard_Light}) format('woff');
     font-weight: 300;
   }
   @font-face {
     font-family: "Pretendard_Regular";
-    src: url("Pretendard"), url(${Pretendard_Regular}) format('otf');
+    src: url("Pretendard"), url(${Pretendard_Regular}) format('woff');
     font-weight: 400;
   }
   @font-face {
     font-family: "Pretendard_Medium";
-    src: url("Pretendard"), url(${Pretendard_Medium}) format('otf');
+    src: url("Pretendard"), url(${Pretendard_Medium}) format('woff');
     font-weight: 500;
   }
   @font-face {
     font-family: "Pretendard_SemiBold";
-    src: url("Pretendard"), url(${Pretendard_SemiBold}) format('otf');
+    src: url("Pretendard"), url(${Pretendard_SemiBold}) format('woff');
     font-weight: 600;
   }
   @font-face {
     font-family: "Pretendard_ExtraBold";
-    src: url("Pretendard"), url(${Pretendard_ExtraBold}) format('otf');
+    src: url("Pretendard"), url(${Pretendard_ExtraBold}) format('woff');
     font-weight: 700;
   }
 `;
