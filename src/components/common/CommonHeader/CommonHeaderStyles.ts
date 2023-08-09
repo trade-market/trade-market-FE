41614aa03011de140ce36df77cef113433e4a86a
haizellatte <hyoeun5000@naver.com>
2023-08-09 17:12:42 +0900

    <feat> Add Home folder

--- src/components/common/CommonHeader/CommonHeaderStyles.ts
+++ src/components/common/CommonHeader/CommonHeaderStyles.ts
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
+import { size } from '../../../styles/theme';
 
 export const Container = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
+  max-width: ${size.mobile};
   min-height: 60px;
+  z-index: 10;
+  position: fixed;
   top: 0;
   font-weight: 500;
   background-color: ${({ theme }) => theme.color.bgColor};
--- src/components/common/Header/HeaderStyles.ts
+++ src/components/common/Header/HeaderStyles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   width: 100%;
   display: flex;
-  margin: 0 30px;
+  padding: 0 30px;
 
   .location {
     align-items: flex-start;
