e7a59922fcf5ce05cce0b4536640f72a5e8219b9
Yongveloper <aaa9015@naver.com>
2023-08-09 18:44:57 +0900

    fix: 경로 못 찾는 오류 해결 폴더, 파일명 한글 -> 영어

--- src/components/common/NavigationBar/NavigationBar.tsx
+++ src/components/common/NavigationBar/NavigationBar.tsx
@@ -1,21 +1,21 @@
 import { Container } from './NavigationBarStyles';
-import chat from '../../../Assets/Icons/홈 화면/채팅_비활성화.svg';
-import home from '../../../Assets/Icons/홈 화면/홈_비활성화.svg';
-import my from '../../../Assets/Icons/홈 화면/MY_비활성화.svg';
+import chat from '../../../Assets/Icons/HomeScreen/Chat_Disable.svg';
+import home from '../../../Assets/Icons/HomeScreen/Home_Disable.svg';
+import my from '../../../Assets/Icons/HomeScreen/MY_Disable.svg';
 
 const NavigationBar = () => {
   return (
     <Container>
-      <div className='item'>
-        <img className='icon' src={chat} />
+      <div className="item">
+        <img className="icon" src={chat} />
         <div>채팅</div>
       </div>
-      <div className='item'>
-        <img className='icon' src={home} />
+      <div className="item">
+        <img className="icon" src={home} />
         <div>홈</div>
       </div>
-      <div className='item'>
-        <img className='icon' src={my} />
+      <div className="item">
+        <img className="icon" src={my} />
         <div>MY</div>
       </div>
     </Container>
