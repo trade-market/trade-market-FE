d2dfcebcc47183f143ab6269562d453ad04c83a8
haizellatte <hyoeun5000@naver.com>
2023-08-10 09:12:46 +0900

    <fix>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import { theme } from './styles/theme';
-import Layout from './components/common/Layout/Layout';
-import Home from "./Pages/Home/Home";
-
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <Layout>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />}  />
-          </Routes>
-        </BrowserRouter>
-      </Layout>
-    </ThemeProvider>
-  );
+  return <div>App</div>;
 }
 
 export default App;
