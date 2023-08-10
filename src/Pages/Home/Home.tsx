a3e711fa1d3a164c9cfe005cff951666fe4472a9
haizellatte <hyoeun5000@naver.com>
2023-08-10 16:52:26 +0900

    <feat> swiper 구현

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 import * as H from "./HomeStyles";
-import Swiper from "./Swiper/Swiper";
+import SwiperBanner from "./Swiper/SwiperBanner";
 import ExchangeMenu from "./Exchange/ExchangeMenu/ExchangeMenu";
 import WriteButton from "./WriteButton/WriteButton";
 
 const Main = () => {
   return (
     <H.Wrapper>
-      <Swiper />
+      <SwiperBanner />
       <ExchangeMenu />
       
       <WriteButton />
