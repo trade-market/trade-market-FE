e3e4d4f31f0784cdcc8901f587458e76ccf6567e
haizellatte <hyoeun5000@naver.com>
2023-08-14 19:53:10 +0900

    <style> headerStyles padding 제거

--- src/components/Search/NeighborhoodSearch/NeighborhoodSearch.tsx
+++ src/components/Search/NeighborhoodSearch/NeighborhoodSearch.tsx
@@ -9,12 +9,12 @@ interface INeighborhoodSearchProps {
 const NeighborhoodSearch = () => {
   //Todo : API 요청을 통해 많이 찾는 거래 배열 가져온다. (useState, useEffect)
   const arr = ['밥솥', '밥솥', '밥솥', '밥솥', '밥솥', '밥솥', '밥솥', '마우스','마우스','마우스','마우스', '보조배터리', '에어프라이어', '갤럭시 워치 5', '보조배터리', '에어프라이어', '갤럭시 워치 5','시디즈 의자', '제습기', '컨버스', '그라인더', '텀블러', '보조배터리', '에어프라이어', '갤럭시 워치 5', '시디즈 의자', '제습기', '컨버스', '그라인더', '텀블러'];
-  const checkRanking = arr.reduce((acc, cur): INeighborhoodSearchProps => {
-    acc[cur] = (acc[cur] || 0) + 1;
-    return acc;
-  }, {});
-  const sorted = Object.entries(checkRanking).sort(([a1, b1], [a1, b2]) => b2 - b1);
-  const popularArr = sorted.map(v => v[0]);
+  // const checkRanking = arr.reduce((acc, cur): INeighborhoodSearchProps => {
+  //   acc[cur] = (acc[cur] || 0) + 1;
+  //   return acc;
+  // }, {});
+  // const sorted = Object.entries(checkRanking).sort(([a1, b1], [a1, b2]) => b2 - b1);
+  // const popularArr = sorted.map(v => v[0]);
   
   return (
     <S.Wrapper>
