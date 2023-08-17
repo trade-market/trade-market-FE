53e660d6645bc4373445c7552ca6550c5fd7535b
Yongveloper <aaa9015@naver.com>
2023-08-17 21:42:39 +0900

    feat: 주소 검색 기반 지도 표시 기능 추가

--- src/components/ProfileSetup/AddressSetting.tsx
+++ src/components/ProfileSetup/AddressSetting.tsx
@@ -2,12 +2,24 @@ import { RootState } from '@store/types';
 import { useSelector } from 'react-redux';
 import BigTitle from '@components/common/BigTitle';
 import * as P from '@Pages/ProfileSetup/ProfileSetupStyles';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
+import { useDaumPostcodePopup, Address } from 'react-daum-postcode';
 function AddressSetting() {
   const address = useSelector(
     (state: RootState) => state.profileAddress.address
   );
+  const navigate = useNavigate();
+  const open = useDaumPostcodePopup();
+
+  const handleComplete = (data: Address) => {
+    navigate('set-location', { state: { address: data.address } });
+  };
+
+  const handleSearchBtnClick = () => {
+    open({ onComplete: handleComplete });
+  };
+
   return (
     <P.Section>
       <BigTitle>동네 설정</BigTitle>
@@ -18,7 +30,9 @@ function AddressSetting() {
           value={address}
           disabled={true}
         />
-        <P.Button disabled={false}>검색</P.Button>
+        <P.Button disabled={false} onClick={handleSearchBtnClick}>
+          검색
+        </P.Button>
       </P.InputContainer>
       <Link to="set-location">
         <P.CurrentLocationBtn>현재 동네로 설정</P.CurrentLocationBtn>
--- src/components/ProfileSetup/SetLocation/KakaoMap/KakaoMap.tsx
+++ src/components/ProfileSetup/SetLocation/KakaoMap/KakaoMap.tsx
@@ -8,17 +8,42 @@ import * as K from './KakaoMapStyles';
 import useCurrentPosition from '@/hooks/useCurrentPosition';
 import CurrentLocation from '@components/ProfileSetup/SetLocation/CurrentLocation/CurrentLocation';
 import SetCurrentLocationBtn from '@components/ProfileSetup/SetLocation/SetCurrentLocationBtn';
+import { useLocation } from 'react-router-dom';
+
+interface LocationState {
+  address: string;
+}
 
 function KakaoMap() {
   const container = useRef<HTMLDivElement>(null);
   const currentPosition = useCurrentPosition();
   const dispatch = useDispatch();
+  const location = useLocation();
+  const locationState = location.state as LocationState | undefined;
 
-  // 좌표 -> 주소 변환 함수
+  // 맵(지도) 생성
+  const createMap = (coord: kakao.maps.LatLng) => {
+    const options = {
+      center: coord,
+      level: 2,
+    };
+    const map = new kakao.maps.Map(container.current as HTMLElement, options);
+    return map;
+  };
+
+  // 지도에 마커 표시, 중심 좌표로 이동
+  const displayMarker = (map: kakao.maps.Map, coord: kakao.maps.LatLng) => {
+    const marker = new kakao.maps.Marker({ position: coord });
+    marker.setMap(map);
+    map.setCenter(coord);
+  };
+
+  // 좌표 -> 주소 획득
   const geoCoordToAddress = (coord: kakao.maps.LatLng) => {
     const geocoder = new kakao.maps.services.Geocoder();
-
-    geocoder.coord2Address(coord.getLng(), coord.getLat(), (result, status) => {
+    const longitude = coord.getLng();
+    const latitude = coord.getLat();
+    geocoder.coord2Address(longitude, latitude, (result, status) => {
       if (status === kakao.maps.services.Status.OK) {
         const { region_1depth_name, region_2depth_name, region_3depth_name } =
           result[0].address;
@@ -27,47 +52,85 @@ function KakaoMap() {
             `${region_1depth_name} ${region_2depth_name} ${region_3depth_name}`
           )
         );
-        dispatch(
-          setProfileCoordinates({
-            latitude: coord.getLat(),
-            longitude: coord.getLng(),
-          })
-        );
+        dispatch(setProfileCoordinates({ longitude, latitude }));
       }
     });
   };
 
-  const initializeMap = () => {
+  // 현재 위치 기반으로 지도 표시
+  const displayMapBasedOnCurrentPosition = () => {
     if (container.current && currentPosition) {
       const latLng = new kakao.maps.LatLng(
         currentPosition.latitude,
         currentPosition.longitude
       );
-      const options = {
-        center: latLng,
-        level: 3,
-      };
-      const map = new kakao.maps.Map(container.current, options);
-
-      // 현재 위치에 마커를 표시하는 함수
-      const displayCurrentLocationMarker = () => {
-        const marker = new kakao.maps.Marker({ position: latLng });
-        marker.setMap(map);
-        map.setCenter(latLng);
-      };
-
-      displayCurrentLocationMarker();
+      const map = createMap(latLng);
+      displayMarker(map, latLng);
       geoCoordToAddress(latLng);
     }
   };
 
+  // 주소 -> 좌표 획득
+  const getCoordinatesFromAddress = (
+    address: string
+  ): Promise<kakao.maps.LatLng | null> => {
+    return new Promise((resolve) => {
+      const geocoder = new kakao.maps.services.Geocoder();
+      geocoder.addressSearch(address, (result, status) => {
+        if (status === kakao.maps.services.Status.OK) {
+          const {
+            x,
+            y,
+            region_1depth_name,
+            region_2depth_name,
+            region_3depth_name,
+          } = result[0].address;
+          const latitude = Number(y);
+          const longitude = Number(x);
+          const coords = new kakao.maps.LatLng(latitude, longitude);
+          dispatch(
+            setProfileAddress(
+              `${region_1depth_name} ${region_2depth_name} ${region_3depth_name}`
+            )
+          );
+          dispatch(setProfileCoordinates({ latitude, longitude }));
+          resolve(coords);
+        } else {
+          resolve(null);
+        }
+      });
+    });
+  };
+
+  // 사용자 입력 주소 기반으로 지도 표시
+  const displayMapBasedOnAddress = async () => {
+    if (!locationState?.address) {
+      return;
+    }
+
+    const latLng = await getCoordinatesFromAddress(locationState.address);
+    if (!latLng) {
+      console.error(
+        'Failed to getLatLng, Function getCoordinatesFromAddress error'
+      );
+      return;
+    }
+
+    const map = createMap(latLng);
+    displayMarker(map, latLng);
+  };
+
   useEffect(() => {
-    initializeMap();
-  }, [initializeMap]);
+    if (locationState?.address) {
+      displayMapBasedOnAddress();
+    } else {
+      displayMapBasedOnCurrentPosition();
+    }
+  }, [displayMapBasedOnCurrentPosition, displayMapBasedOnAddress]);
 
   return (
     <K.Container ref={container}>
-      <SetCurrentLocationBtn onClick={initializeMap} />
+      <SetCurrentLocationBtn onClick={displayMapBasedOnCurrentPosition} />
       <CurrentLocation />
     </K.Container>
   );
