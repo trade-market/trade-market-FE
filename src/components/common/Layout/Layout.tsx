a988104e7863f193fbfda46f566603afb384d05c
Yongveloper <aaa9015@naver.com>
2023-08-21 15:17:30 +0900

    refactor: Layout hide components 로직 수정 articles 페이지를 추가하면서 정규식을 필요로 하는 상황

--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -4,37 +4,34 @@ import Header from '@components/common/Header/Header';
 import NavigationBar from '@components/common/NavigationBar/NavigationBar';
 import WriteButton from '@/components/Home/WriteButton/WriteButton';
 import { useWindowSize } from '@hooks/useWindowDimensions';
+
 interface ILayoutProps {
   children: React.ReactNode;
 }
 
 function Layout({ children }: ILayoutProps) {
   const currentPath = window.location.pathname;
-  const hideHeader = [
-    '/auth',
-    '/profile-setup',
-    '/profile-setup/set-location',
-    '/search',
-  ].includes(currentPath);
-  const hideNavigation = [
-    '/auth',
-    '/profile-setup',
-    '/profile-setup/set-location',
-  ].includes(currentPath);
-  const hideWriteButton = [
-    '/auth',
-    '/profile-setup',
-    '/profile-setup/set-location',
-  ].includes(currentPath);
+  const shouldHideComponent = (regex: RegExp) => regex.test(currentPath);
+
+  const hiddenPaths = [
+    /^\/auth/,
+    /^\/profile-setup/,
+    /^\/search/,
+    /^\/articles\/\d+$/, // /articles/:id
+  ];
+
+  const isHidden = hiddenPaths.some((pathRegex) =>
+    shouldHideComponent(pathRegex)
+  );
   const { height } = useWindowSize();
 
   return (
     <Wrapper height={height}>
       <GlobalStyle />
-      {!hideHeader && <Header />}
+      {!isHidden && <Header />}
       {children}
-      {!hideWriteButton && <WriteButton />}
-      {!hideNavigation && <NavigationBar />}
+      {!isHidden && <WriteButton />}
+      {!isHidden && <NavigationBar />}
     </Wrapper>
   );
 }
