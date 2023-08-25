e102cc091aa3a532e77a4b2bbd63bb14d97794ed
Yongveloper <aaa9015@naver.com>
2023-08-25 15:31:22 +0900

    feat: 로딩 컴포넌트 구현

---
+++ src/components/Auth/Spinner.tsx
@@ -0,0 +1,39 @@
+import styled, { keyframes } from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  min-height: inherit;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const rotation = keyframes`
+  0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+`;
+
+const Loader = styled.div`
+  width: 48px;
+  height: 48px;
+  border: 5px solid ${({ theme }) => theme.color.whiteGray};
+  border-bottom-color: ${({ theme }) => theme.color.Mainblue};
+  border-radius: 50%;
+  display: inline-block;
+  box-sizing: border-box;
+  animation: ${rotation} 1s linear infinite;
+`;
+
+function Spinner() {
+  return (
+    <Container>
+      <Loader />
+    </Container>
+  );
+}
+
+export default Spinner;
