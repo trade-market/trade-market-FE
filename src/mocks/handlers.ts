6d623ee39f6053ec55783f1e7dd13e11b41c6314
Yongveloper <aaa9015@naver.com>
2023-09-05 23:03:46 +0900

    test: OAuth login api mocking

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,16 +1,63 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.post('/api/login', (req, res, ctx) => {
+  rest.get('/oauth2/callback/google', (req, res, ctx) => {
     return res(
+      ctx.set('Authorization', 'Baerer abcdefg12345'),
+      ctx.set('refresh', 'testrefresh12345'),
       ctx.json({
+        code: 200,
+        message: '로그인 성공',
+        isNew: false,
         user: {
           id: '123',
-          name: 'Test User',
-          email: 'test@example.com',
+          nickname: '거래왕',
+          coordinate: { lat: '37.123456', lng: '127.123456' },
+          grade: 'one',
+          town: '경기도 수원시 화서동',
         },
-        token: 'abcdefg12345',
       })
     );
+  }),
+
+  rest.get('/oauth2/callback/kakao', (req, res, ctx) => {
+    // 신규 유저일 경우
+    return res(
+      ctx.json({
+        code: 200,
+        message: '신규 유저',
+        isNew: true,
+        user: {
+          auth_id: '123',
+          nickname: '거래왕',
+          profile_image:
+            'https://static.wanted.co.kr/images/events/1633/f85834e9.jpg',
+        },
+      })
+    );
+  }),
+
+  rest.get('/oauth2/callback/naver', (req, res, ctx) => {
+    // 로그인 성공시
+    return res(
+      ctx.set('Authorization', 'Baerer abcdefg12345'),
+      ctx.set('refresh', 'testrefresh12345'),
+      ctx.json({
+        code: 200,
+        message: '로그인 성공',
+        isNew: false,
+        user: {
+          id: '123',
+          nickname: '거래왕',
+          coordinate: { lat: '37.123456', lng: '127.123456' },
+          grade: 'one',
+          town: '경기도 수원시 화서동',
+        },
+      })
+    );
+  }),
+
+  rest.post('/auth/token', (req, res, ctx) => {
+    return res(ctx.set('Authorization', 'Baerer newtoken123124'));
   }),
 ];
