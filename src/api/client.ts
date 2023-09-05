33901bb21c54b5da4d51dc628698ae9810d7ae55
Yongveloper <aaa9015@naver.com>
2023-09-05 23:02:13 +0900

    feat: token interceptor api주소 추후에 바꿔야함

---
+++ src/api/client.ts
@@ -0,0 +1,46 @@
+import axios, { AxiosError } from 'axios';
+import TokenService from '@/service/TokenService';
+
+export const BASE_URL = 'http://localhost:5173'; // Todo: 서버 배포되면 환경변수로 설정
+
+const client = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+});
+
+client.interceptors.request.use((config) => {
+  const accessToken = TokenService.getAccessToken();
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+client.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = TokenService.getRefreshToken();
+
+      try {
+        const res = await client.post('/auth/token', { refreshToken });
+        const newAccessToken = res.headers['authorization'].split(' ')[1];
+        TokenService.setAccessToken(newAccessToken);
+        return client(originalRequest);
+      } catch (err) {
+        const axiosErr = err as AxiosError;
+        if (axiosErr.response && axiosErr.response.status === 403) {
+          TokenService.clearTokens();
+          window.location.href = '/';
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default client;
