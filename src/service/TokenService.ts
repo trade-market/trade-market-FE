909870e8e38b35407c58938e053c90f08b32fb8d
Yongveloper <aaa9015@naver.com>
2023-09-11 23:58:18 +0900

    feat: 쿠키 옵션 추가

--- src/service/TokenService.ts
+++ src/service/TokenService.ts
@@ -5,7 +5,10 @@ class TokenService {
     localStorage.setItem('accessToken', accessToken);
 
   setRefreshToken = (refreshToken: string) =>
-    Cookies.set('refreshToken', refreshToken);
+    Cookies.set('refreshToken', refreshToken, {
+      secure: import.meta.env.PROD,
+      sameSite: 'lax',
+    });
 
   getAccessToken = () => localStorage.getItem('accessToken');
 
