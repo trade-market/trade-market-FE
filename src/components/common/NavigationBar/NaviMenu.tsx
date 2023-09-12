e3e64d4b3dae958d9d7e955e20796dd87a1d80c0
Yongveloper <aaa9015@naver.com>
2023-09-12 23:33:43 +0900

    feat: navbar 페이지 이동 추가

--- src/components/common/NavigationBar/NaviMenu.tsx
+++ src/components/common/NavigationBar/NaviMenu.tsx
@@ -9,21 +9,26 @@ interface INaviMenuProps {
   menu: string;
 }
 
-const NaviMenu = ({ able, disable, activeNav, position, setActiveNav, menu }: INaviMenuProps) => {
+const NaviMenu = ({
+  able,
+  disable,
+  activeNav,
+  position,
+  setActiveNav,
+  menu,
+}: INaviMenuProps) => {
   const navigate = useNavigate();
 
   const handleNavigation = () => {
-    navigate('/');
+    if (position === 2) navigate('/');
+    if (position === 3) navigate('/my-page');
     setActiveNav(position);
   };
-  
+
   return (
-    <div
-      className="item"
-      onClick={handleNavigation}
-      >
+    <div className="item" onClick={handleNavigation}>
       <img className="icon" src={activeNav === position ? able : disable} />
-      <div className={activeNav === position ? "active" : ""}>{menu}</div>
+      <div className={activeNav === position ? 'active' : ''}>{menu}</div>
     </div>
   );
 };
