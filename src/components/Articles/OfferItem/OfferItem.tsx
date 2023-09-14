04abda2cf018034ea29e3381a3084045752db9a8
Yongveloper <aaa9015@naver.com>
2023-09-14 22:32:57 +0900

    style: 오퍼 컨테이너 border 적용

--- src/components/Articles/OfferItem/OfferItem.tsx
+++ src/components/Articles/OfferItem/OfferItem.tsx
@@ -2,7 +2,6 @@ import * as O from './OfferItemStyles';
 import RatingBadge from '@components/common/RatingBadge';
 import optionDot from '@Assets/offer/Detailed-page/option_dot.svg';
 import linkIcon from '@Assets/offer/Detailed-page/link.svg';
-import BigTitle from '@components/common/BigTitle';
 import useTimeDiff from '@hooks/useTimeDiff';
 import { OfferPostTypes } from '@/types/OfferTypes';
 
