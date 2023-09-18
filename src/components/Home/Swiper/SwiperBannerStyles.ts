4c76fdde841ef1a0567f67751b3e107107d3cb1c
Yongveloper <aaa9015@naver.com>
2023-09-18 23:02:45 +0900

    fix: 배경색 짤림 해결

--- src/components/Home/Swiper/SwiperBannerStyles.ts
+++ src/components/Home/Swiper/SwiperBannerStyles.ts
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
   width: 100%;
   padding-top: 60px;
-  background-color: ${({ theme }) => theme.color.Mainblue};
+  background-color: #007af7;
 
   .slide-item {
     display: flex;
