92eecb9444fe843f0ed5bf10c96915069c1a9731
Yongveloper <aaa9015@naver.com>
2023-09-18 21:31:26 +0900

    feat: CommonModal 컴포넌트 구현

---
+++ src/components/common/CommonModal.tsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import ModalBackground from './ModalBackground';
+
+const ModalWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 300px;
+  width: 100%;
+  height: 203px;
+  z-index: 999;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.color.bgColor};
+  box-shadow: 0px 0px 12px 0px rgba(33, 86, 242, 0.1);
+
+  .title {
+    font-size: ${({ theme }) => theme.font.size.medium};
+    font-weight: 600;
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 27px;
+  }
+
+  .close-btn {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    color: ${({ theme }) => theme.color.white};
+    background-color: ${({ theme }) => theme.color.Mainblue};
+    padding: 11px;
+    border: none;
+  }
+`;
+
+interface ICommonModalProps {
+  isOpen: boolean;
+  title: string;
+  closeAction: () => void;
+}
+
+function CommonModal({ isOpen, title, closeAction }: ICommonModalProps) {
+  if (!isOpen) return null;
+  return (
+    <>
+      <ModalBackground />
+      <ModalWrapper>
+        <h1 className="title">{title}</h1>
+        <button className="close-btn" onClick={closeAction}>
+          확인
+        </button>
+      </ModalWrapper>
+    </>
+  );
+}
+
+export default CommonModal;
--- src/components/common/ConfirmModal.tsx
+++ src/components/common/ConfirmModal.tsx
@@ -1,16 +1,6 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-const ModalBackground = styled.div`
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  z-index: 998;
-  background: rgba(0, 0, 0, 0.2);
-  backdrop-filter: blur(2px);
-`;
+import ModalBackground from './ModalBackground';
 
 const ModalWrapper = styled.div`
   position: fixed;
