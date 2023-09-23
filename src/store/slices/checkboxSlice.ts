29af69ffbc7e0170735a54d47ee84c2fa8b65b7d
Yongveloper <aaa9015@naver.com>
2023-09-23 16:37:18 +0900

    style: 주석 추가

--- src/store/slices/checkboxSlice.ts
+++ src/store/slices/checkboxSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CheckboxState {
   checkboxVisible: boolean;
-  checkedItems: string[]; //
+  checkedItems: string[];
 }
 
 const checkBoxSlice = createSlice({
@@ -12,20 +12,29 @@ const checkBoxSlice = createSlice({
     checkedItems: [],
   } as CheckboxState,
   reducers: {
+    // 체크 박스 보이기/숨기기
     toggleCheckboxVisible(state) {
       state.checkboxVisible = !state.checkboxVisible;
     },
+    // 체크 박스 숨기기
     inVisibleCheckbox(state) {
       state.checkboxVisible = false;
     },
+    // 체크 박스 체크
     checkItem(state, action: PayloadAction<string>) {
       state.checkedItems.push(action.payload);
     },
+    // 체크 박스 해제
     uncheckItem(state, action: PayloadAction<string>) {
       state.checkedItems = state.checkedItems.filter(
         (id) => id !== action.payload
       );
     },
+    // 체크 박스 전체 체크
+    checkAll(state, action: PayloadAction<string[]>) {
+      state.checkedItems = [...action.payload];
+    },
+    // 체크 박스 전체 해제
     resetCheckedItems(state) {
       state.checkedItems = [];
     },
@@ -37,6 +46,7 @@ export const {
   inVisibleCheckbox,
   checkItem,
   uncheckItem,
+  checkAll,
   resetCheckedItems,
 } = checkBoxSlice.actions;
 
