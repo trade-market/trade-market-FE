2c97e820c179d62a8a859ac63c50b45ed3b08d1d
Yongveloper <aaa9015@naver.com>
2023-10-23 19:16:02 +0900

    feat: Signup 로직 RTK Query 사용

--- src/Pages/SingUp/SignUp.tsx
+++ src/Pages/SingUp/SignUp.tsx
@@ -1,6 +1,5 @@
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
-import AuthService from '@/service/AuthService';
-import { Coordinates, NewUserInfo } from '@/types/UserTypes';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Coordinates } from '@/types/UserTypes';
 import defaultProfileImg from '@Assets/Images/default_profile.svg';
 import ProfileSetupForm from '@components/common/ProfileSetupForm';
 import { useDispatch } from 'react-redux';
@@ -9,11 +8,15 @@ import UserService from '@/service/UserService';
 import CommonModal from '@components/common/CommonModal';
 import useModal from '@hooks/useModal';
 import SignUpSuccessModal from '@components/Signup/SignUpSuccessModal';
+import { NewUserResponse } from '@/types/AuthTypes';
+import { useSignUpMutation } from '@store/slices/authApiSlice';
+import Spinner from '@components/Auth/Spinner';
 
 function SignUp() {
+  const [signUp, { isLoading: isSingUpLoading }] = useSignUpMutation();
   const navigate = useNavigate();
   const location = useLocation();
-  const state = location.state as NewUserInfo;
+  const state = location.state as NewUserResponse;
   const { isOpen, open } = useModal();
 
   const handleModalOkClick = () => {
@@ -30,8 +33,25 @@ function SignUp() {
     );
   }
   const dispatch = useDispatch();
-  const nickname = state.nickname; // Todo: SNS 로그인 시 닉네임 받아오기
-  const profileImg = state.profile_image; // Todo: SNS 로그인 시 프로필 이미지 받아오기
+  const { authId, authType, nickname, profileImage } = state;
+
+  const handleSuccessfulSignUp = async () => {
+    const user = await UserService.getUserInfo();
+    open();
+    setTimeout(() => {
+      dispatch(setUser({ ...user, isLogin: true }));
+      navigate('/', { replace: true });
+      console.log('tt');
+    }, 3000);
+  };
+
+  const handleError = (error: any) => {
+    console.error(error);
+    alert('회원가입에 실패했습니다. error: ' + error);
+    navigate('/auth', { replace: true });
+  };
+
+  // Todo: API 명세서에는 town을 받지 않게 되어 있는데 어떻게 할 것인지?
   const handleSubmit = async (
     nickname: string,
     coordinates: Coordinates,
@@ -39,33 +59,36 @@ function SignUp() {
     profileImgFile: File | null
   ) => {
     const userInfo = {
+      authId,
+      authType,
       nickname,
-      profileImg,
-      profileImgFile,
-      coordinates,
-      town,
+      profileImage,
+      imageFile: profileImgFile,
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude,
     };
     try {
-      const data = await AuthService.signUp(userInfo);
-      const { user } = await UserService.getUserInfo();
-      open();
-      setTimeout(() => {
-        dispatch(setUser({ ...user, isLogin: true }));
-        navigate('/', { replace: true });
-      }, 3000);
-    } catch (err) {
-      console.error(err);
+      const result = await signUp(userInfo).unwrap();
+      console.log(result);
+      if (result.code === 201) {
+        await handleSuccessfulSignUp();
+      } else if (result.code === 400) {
+        throw new Error(result.message);
+      }
+    } catch (error) {
+      handleError(error);
     }
   };
 
   return (
     <>
       <ProfileSetupForm
-        defaultProfileImgSrc={profileImg || defaultProfileImg}
+        defaultProfileImgSrc={profileImage || defaultProfileImg}
         defaultNickname={nickname}
         handleSubmit={handleSubmit}
       />
       <SignUpSuccessModal isOpen={isOpen} />
+      {isSingUpLoading && <Spinner />}
     </>
   );
 }
