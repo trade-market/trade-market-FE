63e6fd7a3981ef9c59dcf20c4e383c4270e67251
Yongveloper <aaa9015@naver.com>
2024-02-19 18:20:57 +0900

    refactor: moment -> date-fns 사용 날짜 관련 라이브러리 중복 설치된 문제 및 패키지 용량 감소 효과 및 가독성 Up

--- src/hooks/useTimeDiff.ts
+++ src/hooks/useTimeDiff.ts
@@ -1,46 +1,15 @@
-import { useState, useEffect } from 'react';
-import moment from 'moment';
+import { formatDistance } from 'date-fns';
+import { ko } from 'date-fns/locale';
 
 const useTimeDiff = (date: Date | string) => {
-  const [timeDiff, setTimeDiff] = useState('');
+  const now = new Date();
+  const postDate = new Date(date);
+  const result = formatDistance(postDate, now, {
+    addSuffix: true,
+    locale: ko,
+  });
 
-  useEffect(() => {
-    const calculateTimeDiff = () => {
-      const now = moment();
-      const postDate = moment(date);
-      const diffInSeconds = now.diff(postDate, 'seconds');
-      const diffInMinutes = now.diff(postDate, 'minutes');
-      const diffInHours = now.diff(postDate, 'hours');
-      const diffInDays = now.diff(postDate, 'days');
-      const diffInMonths = now.diff(postDate, 'months');
-      const diffInYears = now.diff(postDate, 'years');
-
-      let result = '';
-
-      if (diffInSeconds < 60) {
-        result = `${diffInSeconds}초 전`;
-      } else if (diffInMinutes < 60) {
-        result = `${diffInMinutes}분 전`;
-      } else if (diffInHours < 24) {
-        result = `${diffInHours}시간 전`;
-      } else if (diffInDays < 30) {
-        result = `${Math.floor(diffInDays)}일 전`;
-      } else if (diffInMonths < 12) {
-        result = `${Math.floor(diffInMonths)}개월 전`;
-      } else {
-        result = `${Math.floor(diffInYears)}년 전`;
-      }
-
-      setTimeDiff(result);
-    };
-
-    calculateTimeDiff();
-    const timerId = setInterval(calculateTimeDiff, 60000); // 1 분마다 업데이트
-
-    return () => clearInterval(timerId); // cleanup
-  }, [date]);
-
-  return timeDiff;
+  return result;
 };
 
 export default useTimeDiff;
